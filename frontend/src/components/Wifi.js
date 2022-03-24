4279a8e953952131302ff9870f602e304767908d
Vili Raunola <raunolavili@gmail.com>
2022-03-24 15:12:06 +0200

    Getting the data from api works. Inserting to table works

--- frontend/src/components/Wifi.js
+++ frontend/src/components/Wifi.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import WifiTable from './WifiTable'
+import {Box} from '@mui/system'
+import { Container } from '@mui/material'
 
 function Wifi() {
-  return (
-    <div>Wifi</div>
-  )
+
+  const [data, setData] = useState([{}])   //Wifi data fetched from the server
+
+  //Fetching data from the api
+  //Source for setting up useEffect with timer: https://stackoverflow.com/questions/67463964/react-useeffect-and-setinterval
+  useEffect(() => {
+    const getWifiData = () => {
+      fetch('data/wifi')
+      .then(res => res.json())
+      .then(data => {
+        setData(data.wifi)
+      })
+    }
+   
+    //Getting the data on landing the page
+    getWifiData();
+    //Setting an interval for 10 s.
+    const interval = setInterval(() => {
+      getWifiData()
+    }, 10000);
+    return () => clearInterval(interval);
+  }, [])
+
+  console.log(data)
+
+  if(data.length < 2){
+    return <div>Loading...</div>
+  }else {
+    return (
+      <Container>
+        <Box sx={{ m: 3}}>
+          <WifiTable data_list={data}/>
+        </Box>
+      </Container>
+      
+    )
+  }
 }
 
 export default Wifi
