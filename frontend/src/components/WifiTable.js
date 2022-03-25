b1e8815723f8fd514d0aca7a6e72b4140622581a
Vili Raunola <raunolavili@gmail.com>
2022-03-25 12:58:17 +0200

    removed More header from tables

--- frontend/src/components/WifiTable.js
+++ frontend/src/components/WifiTable.js
@@ -41,9 +41,6 @@ function Row(props) {
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 ,background: row.Is_AP ? '#90EE90': '#FFFFE0'}} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                More
-              </Typography>
               <Table size="small" aria-label="purchases">
                 <TableHead>
                   <TableRow>
